1635175fb8f9404408108a832c41d0c35d2c5778
Antonio Gabriel <antoniocamposgabriel@gmail.com>
2023-08-18 00:25:47 +0100

    feat: create db to store exchanges

---
+++ src/lib/database/database.ts
@@ -0,0 +1,19 @@
+import path from 'path'
+import fs from 'node:fs/promises'
+import { CurrencyData } from 'application/contracts/transformation-contract'
+
+export abstract class Database {
+  static $filePath: string = path.join(__dirname, 'exchanges.json')
+
+  static async insert_bulk(data: CurrencyData[]) {
+    const convertToString = JSON.stringify(data, null, 2)
+
+    await fs.writeFile(this.$filePath, convertToString)
+    console.info('[SUCCESS]: Exchanges saved into json file successfully!')
+  }
+
+  static async get() {
+    const fileContents = await fs.readFile(this.$filePath, 'utf-8')
+    return JSON.parse(fileContents) ?? []
+  }
+}
