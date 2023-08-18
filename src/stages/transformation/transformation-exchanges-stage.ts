1be5a1e481701f1b68caec8365e69c2e93fa21a7
Antonio Gabriel <antoniocamposgabriel@gmail.com>
2023-08-18 20:59:15 +0100

    refactor: adding validator to identify invalid quote code

--- src/stages/transformation/transformation-exchanges-stage.ts
+++ src/stages/transformation/transformation-exchanges-stage.ts
@@ -9,36 +9,45 @@ export class TransformationExchangesStage {
   constructor(private readonly extractedData: ExtractionStageContract) {}
 
   transform(): TransformationStageContract {
-    if (this.extractedData.data.length > 0) {
-      const exchanges: CurrencyData[] = []
-
-      this.extractedData.data.forEach(([source, data]) => {
-        exchanges.push({
-          source,
-          quotes: [],
-        })
-
-        data.forEach((_exchange) => {
-          const $ = cheerio.load(_exchange as string)
-          const exchange = $('.YMlKec.fxKbKc').text()
-          const quote = $('.PdOqHc').text().split('/')[1].trim().substring(0, 3)
-
-          const exchangeIndex = exchanges.findIndex(
-            (data) => data.source == source,
-          )
-
-          exchanges[exchangeIndex].quotes.push([quote, exchange])
-        })
-      })
-
-      return {
-        data: exchanges,
-        last_update: new Date().toString(),
-      }
+    if (this.extractedData.data.length === 0) {
+      throw new Error(
+        `Something wrong on ${this.extractedData.fallback_error?.stage} stage`,
+      )
     }
 
+    const exchanges: CurrencyData[] = []
+
+    this.extractedData.data.forEach(([source, data]) => {
+      exchanges.push({
+        source,
+        quotes: [],
+      })
+
+      data.forEach((_exchange) => {
+        const $ = cheerio.load(_exchange as string)
+
+        const exchange = $('.YMlKec.fxKbKc').text()
+        const quote = $('.PdOqHc').text()
+
+        const exchangeIndex = exchanges.findIndex(
+          (data) => data.source == source,
+        )
+
+        if (exchangeIndex !== -1) {
+          if (exchange && quote) {
+            quote.split('/')[1].trim().substring(0, 3)
+
+            exchanges[exchangeIndex].quotes.push([quote, exchange])
+          } else {
+            exchanges.splice(exchangeIndex, 1)
+          }
+        }
+      })
+    })
+
     return {
-      data: [],
+      data: exchanges,
+      last_update: new Date().toString(),
     }
   }
 }
